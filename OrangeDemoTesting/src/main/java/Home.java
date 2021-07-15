3fdeb053d07bd1e9a66c2a362d970318b0e0fed9
wizutpal <wizutpal@LAPTOP-KQ19DK7F>
2021-07-15 20:27:53 +0530

    Maven POM Framework

---
+++ OrangeDemoTesting/src/main/java/Home.java
@@ -0,0 +1,18 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Home {
+WebDriver driver;
+By request=By.id("buyBtn1");
+
+public Home(WebDriver driver) {
+	this.driver=driver;
+}
+
+public void clickOnRequest() {
+	driver.findElement(request).click();
+}
+
+}
